8114f788e8d4d2a2874530f8e5e6950326e23c25
Marcov006 <marc.caterini@intel.com>
2017-02-23 11:06:42 -0600

    add if/else and switch case statements
    Signed-off-by: Marcov006 <marc.caterini@intel.com>

--- scripts/Ex_Files_UaR_MATLAB/Exercise Files/Chapter 2/conditionals.m
+++ scripts/Ex_Files_UaR_MATLAB/Exercise Files/Chapter 2/conditionals.m
@@ -1,6 +1,26 @@
 clear;
 a = [1,2,3;2,6,4;1,2,round(rand(1)*5)];
-if a(3,3)==3
+b = det(a);
+disp(a);
+disp(b);
+printf("### conditionals.m  -- IF/ELSE statements\n");
+if det(a)==0
+  printf("determinant = 0 , will transpose matrice\n");
   a = a';
 else
+  printf("determinant != 0 , will invert matrice\n");
   a = inv(a);
+end
+disp(a);
+
+
+printf("### conditionals.m  -- SWITCH/CASE statements\n");
+switch b
+  case 0
+  printf("determinant = 0 , will transpose matrice\n");
+  a = a';
+  otherwise
+  printf("determinant != 0 , will invert matrice\n");
+  a = inv(a);
+end
+disp(a);
